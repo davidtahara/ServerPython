0d27b2a9ab83ba1d4cfb38e678177e6f253b529e
jvbrates <jv.belmonterates@gmail.com>
2024-04-24 14:09:18 -0300

    Adicionado grafo

--- Modules/rodrigo_thierry_joaovitor/routers/trabalho4.py
+++ Modules/rodrigo_thierry_joaovitor/routers/trabalho4.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
-from ...rodrigo_thierry_joaovitor.Parser import PacketSource, UDPPacket, packetSource as src
+from typing import Dict, List, Any
+import json
+from ...rodrigo_thierry_joaovitor.Parser import PacketSource, UDPPacket, IPPacket, packetSource as src
 from ...rodrigo_thierry_joaovitor.PortFinder import findService
 
 router = APIRouter(prefix="/grupo_rodrigo_thierry_joao/udp", tags=[""])
+
 
 @router.get("/todos")
 def get_todos():
@@ -11,10 +14,12 @@ def get_todos():
         if isinstance(packet, UDPPacket):
             yield packet
 
+
 @router.get("/services/{port}")
 def get_services(port: int):
     ''' Retorna todos os serviços que usam uma porta UDP'''
     return findService(port)
+
 
 @router.get("/port/{port}")
 def get_in_port(port: int):
@@ -22,9 +27,68 @@ def get_in_port(port: int):
     for packet in src.allPackets:
         if not isinstance(packet, UDPPacket):
             continue
-        udpPacket:UDPPacket = packet
+        udpPacket: UDPPacket = packet
         if udpPacket.dstPort == port:
             yield udpPacket
+
+
+@router.get("/graph")
+def miserables():
+    pre_nodes: Dict[str, Dict] = dict()
+    edges: List[Dict[str, str]] = []
+
+    allUDP: List[UDPPacket] = src.allPacketsDict[UDPPacket]
+    category: List[str] = []
+
+    for item in allUDP:
+
+        ip: IPPacket = item.external_pdu
+
+        # Categorias Baseadas em IP
+
+        if ip.sourceIp not in category:
+            category.append(ip.sourceIp)
+
+        if ip.destinationIp not in category:
+            category.append(ip.destinationIp)
+
+        # Nodes, IP:PORT
+        src_socket: str = ip.sourceIp + ":" + str(item.srcPort)
+        if src_socket not in pre_nodes:
+            tmp = {"id": src_socket,
+                   "name": src_socket,
+                   "size": 10,
+                   "value": 10,
+                   "symbolSize": 10,
+                   "category": category.index(ip.sourceIp)}
+            pre_nodes[src_socket] = tmp
+
+        dst_socket: str = ip.destinationIp + ":" + str(item.dstPort)
+        if dst_socket not in pre_nodes:
+            pre_nodes[dst_socket] = {"id": dst_socket,
+                                     "name": dst_socket,
+                                     "size": 10,
+                                     "value": 10,
+                                     "symbolSize": 10,
+                                     "category": category.index(ip.destinationIp)}
+
+        # Edges, IP:PORT --> IP:PORT
+
+        edges.append({
+            "source": src_socket,
+            "target": dst_socket
+        })
+
+        # incrementa o campo value/size do nó destino
+        pre_nodes.get(dst_socket)["size"] += 1
+        pre_nodes.get(dst_socket)["value"] += 1
+        pre_nodes.get(dst_socket)["symbolSize"] += 1
+
+
+    retorno: Dict[str, List[Dict[str, Any]]] = {"nodes": list(pre_nodes.values()), "links": edges,
+                                                "categories": [{"name": i} for i in category]}
+
+    return retorno
 
 #
 # Nao tem jeito facil de pegar o ip de cada pacote udp
---
+++ Modules/rodrigo_thierry_joaovitor/views/udp.js
@@ -0,0 +1,65 @@
+
+
+var chartDom = document.getElementById('graph');
+
+var myChart = echarts.init(chartDom);
+var option;
+
+myChart.showLoading();
+$.getJSON('http://127.0.0.1:3001/grupo_rodrigo_thierry_joao/udp/graph', function (graph) {
+  myChart.hideLoading();
+  graph.nodes.forEach(function (node) {
+    node.label = {
+      show: node.symbolSize > 30
+    };
+  });
+  option = {
+    title: {
+      text: '______',
+      subtext: 'Default layout',
+      top: 'bottom',
+      left: 'right'
+    },
+    tooltip: {},
+    legend: [
+      {
+        // selectedMode: 'single',
+        data: graph.categories.map(function (a) {
+          return a.name;
+        })
+      }
+    ],
+    animationDuration: 1500,
+    animationEasingUpdate: 'quinticInOut',
+    series: [
+      {
+        edgeSymbol: ['circle', 'arrow'],
+        name: 'Tráfico UDP',
+        type: 'graph',
+        legendHoverLink: false,
+        layout: 'circular',
+        data: graph.nodes,
+        links: graph.links,
+        categories: graph.categories,
+        roam: true,
+        label: {
+          position: 'right',
+          formatter: '{b}'
+        },
+        lineStyle: {
+          color: 'source',
+          curveness: 0.3
+        },
+        emphasis: {
+          focus: 'adjacency',
+          lineStyle: {
+            width: 10
+          }
+        }
+      }
+    ]
+  };
+  myChart.setOption(option);
+});
+
+option && myChart.setOption(option);
