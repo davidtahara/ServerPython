1044bdb4572e7f941358b880db208aeb9d722327
DeivisFelipe <guerreirodosul01@gmail.com>
2024-05-01 15:17:06 -0300

    fix: arrumando erros grupo diovani_breno_pedro

--- Modules/diovani_breno_pedro/routers/trab1.py
+++ Modules/diovani_breno_pedro/routers/trab1.py
@@ -1,11 +1,16 @@
 import json
 import ipaddress
 from scapy.all import *
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/trabalho1", tags=[""])
 
 def is_public_ip(ip):
     return not ipaddress.ip_address(ip).is_private
 
-def obter_bytes_por_ip(file_path):
+@router.get("/trabalho1")
+def obter_bytes_por_ip():
+    file_path = "./pcaps/trabalho1.pcapng"
     packets = rdpcap(file_path)
 
     bytes_por_ip = {}
@@ -26,7 +31,5 @@ def obter_bytes_por_ip(file_path):
 
     bytes_por_ip = dict(sorted(bytes_por_ip.items(), key=lambda item: item[1]["bytes"], reverse=True))
 
-    with open("../views/trab1.json", "w") as json_file:
+    with open("./Modules/diovani_breno_pedro/views/trab1.json", "w") as json_file:
         json.dump(bytes_por_ip, json_file, indent=4)
-
-obter_bytes_por_ip("../../pcaps/trabalho1.pcapng")
