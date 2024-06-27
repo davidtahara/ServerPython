3f9d7134ab4170a196733e6d2942c55b92642a10
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-06-27 11:18:14 -0300

    WP: remaking T4

--- Modules/pedro_alan_rodrigo_henrique/routers/PcapReader/PcapReader.py
+++ Modules/pedro_alan_rodrigo_henrique/routers/PcapReader/PcapReader.py
@@ -9,7 +9,7 @@ class PcapReader:
 
     def __read_pcap(self):
         try:
-            self.packets = rdpcap(self.file_path, 10)
+            self.packets = rdpcap(self.file_path, 1000)
             return True
         except Exception as e:
             print("Error reading pcap file:", e)
---
+++ Modules/pedro_alan_rodrigo_henrique/routers/T4Udp/T4Udp.py
@@ -0,0 +1,45 @@
+from scapy.all import *
+from Modules.pedro_alan_rodrigo_henrique.routers.RipSniffer.PcapReader import PcapReader
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+class T4Udp:
+    def __init__(self):
+        pcapReader = PcapReader(current_dir + "/udp.pcap")
+        self.packets = pcapReader.get_content()
+
+    def get_data(self):
+        ripNetCount = {}
+        ripNetCount2 = {}
+        routeTag = ""
+        cmd = ""
+        version = ""
+        for packet in self.packets:
+            packet_data = {
+                "srcIp": packet[IP].src,
+                "routeTag": packet[RIP].RouteTag,
+                "cmd": packet[RIP].cmd,
+                "version": packet[RIP].version,
+                "ripNetwork": f"{packet[RIP].addr}{mask_to_cidr(packet[RIP].mask)}",
+            }
+            print(packet_data)
+            # Os seguintes dados são constantes para todos pacotes
+            routeTag = packet_data["routeTag"]
+            cmd = packet_data["cmd"]
+            version = packet_data["version"]
+
+            ripNet = packet_data["ripNetwork"]
+            if packet_data["srcIp"] == "10.0.0.1":
+                ripNetCount[ripNet] = ripNetCount.get(ripNet, 0) + 1
+
+            else:
+                ripNetCount2[ripNet] = ripNetCount2.get(ripNet, 0) + 1
+        return {
+            "router1Data": ripNetCount,
+            "router2Data": ripNetCount2,
+            "routeTag": routeTag,
+            "cmd": cmd,
+            "version": version,
+        }
